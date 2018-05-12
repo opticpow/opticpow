6401b92176a4f74b586c0786b6d35477c29b6a9f
Wayne Ingram <wayne@opticpow.io>
2018-05-12 10:59:12 +1000

    Updated publish script to remove stale files

--- blog/bin/publish.sh
+++ blog/bin/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 hugo
-aws s3 sync --profile personal public/ s3://opticpow.io/
+aws s3 sync --profile personal --delete --storage-class REDUCED_REDUNDANCY public/ s3://opticpow.io/
